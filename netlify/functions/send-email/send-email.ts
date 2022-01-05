c86b6f162ba413fb5f889e312af08dfdbe7b7a94
jdglaser <jarred.glaser@gmail.com>
2022-01-04 22:02:22 -0600

    add email sending

---
+++ netlify/functions/send-email/send-email.ts
@@ -0,0 +1,46 @@
+import { Handler } from '@netlify/functions'
+import { SESClient, SendEmailCommand, SendEmailCommandInput} from "@aws-sdk/client-ses";
+
+function capitalize(val: string) {
+  return val[0].toUpperCase() + val.slice(1);
+}
+
+export const handler: Handler = async (event, context) => {
+  const {person} = event.queryStringParameters;
+  const client = new SESClient({ region: "us-east-2" });
+
+  const params: SendEmailCommandInput = {
+    Source: "robo.jarred@gmail.com",
+    Destination: {ToAddresses: ["jarred.glaser@gmail.com"]},
+    Message: {
+      Subject: {
+        Charset: "UTF-8", 
+        Data: "Hi!"
+      }, 
+      Body: {
+        Html: {
+          Charset: "UTF-8",
+          Data: `<h1>Woo Hoo!🎉</h1><h2>${capitalize(person)} said yes to being in our wedding!</h2>`
+        }
+      }
+    }
+  };
+  const command = new SendEmailCommand(params);
+
+  try {
+    const data = await client.send(command);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Email sent"
+      })
+    }
+  } catch (error) {
+    return {
+      statusCode: error.$metadata.httpStatusCode,
+      body: JSON.stringify({
+        message: "Email failed to send"
+      })
+    }
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import $ from 'jquery';
 const groomsmen = ["evan", "caleb", "justin", "steven", "jimmy", "max", "keegan"] as const;
 type Groomsman = typeof groomsmen[number];
 
+type EmailStatus = "success" | "failure" | null
+
 function capitalize(val: string) {
   return val[0].toUpperCase() + val.slice(1);
 }
@@ -30,19 +32,23 @@ function App() {
 
   const [step, setStep] = useState<number>(0);
   const [countdown, setCountdown] = useState<number>(5);
+  const [emailStatus, setEmailStatus] = useState<EmailStatus>(null);
+  const [showConfetti, setShowConfetti] = useState<boolean>(false);
   
   const person = searchParams.get("awesome-person") as Groomsman
 
   const bottomRef = useRef<null | HTMLDivElement>(null);
 
-  useEffect(() => {
-    fetch('/.netlify/functions/hello-world', {
+  async function sendEmailNotification() {
+    turnOnConfetti();
+    console.log("Sending email")
+    return await fetch(`/.netlify/functions/send-email?person=${person}`, {
       method: 'GET'
     }).then(async response => {
-      const body = await response.json()
-      console.log(body)
+      const status = response.status;
+      setEmailStatus(status === 200 ? "success" : "failure");
     })
-  }, [])
+  }
 
   if (!person || !groomsmen.includes(person)) {
     return (
@@ -66,12 +72,21 @@ function App() {
     }
   }, [step])
 
+  function turnOnConfetti() {
+    setShowConfetti(true);
+    const confettiTimer = setTimeout(() => {
+      setShowConfetti(false);
+      clearTimeout(confettiTimer)
+    }, 5000);
+  }
+
   useEffect(() => {
     if (countdown > 4) {
       return;
     }
 
     if (countdown === -1) {
+      turnOnConfetti();
       return;
     }
 
@@ -94,64 +109,80 @@ function App() {
 
   return (
     <>
-      <h1>Hi {capitalize(person)},</h1>
-      <div className='header'>
-        <h2>I have a very important question for you...</h2>
-        <div className={`countdown ${step >= 5 && countdown !== -1 ? "" : "hide"}`}>
-          {countdown}
-        </div>
-        <div style={{margin: "auto 50%"}}>
-            {countdown === -1 && <ConfettiExplosion />}
-        </div>
-        <div className={`question ${countdown === -1 ? "" : "hide"}`}>
-          <div className='question-text'>
-            <div className='question-text-conf'>
-              <div>🎉</div><div>🎉</div>
-            </div>
+      {emailStatus === null ? null : (
+          <div className='result'>
+            <h2>🎉 WOO HOO! 🎉</h2>
             <div>
-              Will you be {resolveRole(person)}?
-            </div>
-            <div className='question-text-conf'>
-              <div>🎉</div><div>🎉</div>
+              Can't wait to have you as part of our big day, {capitalize(person)}!<br /><br />
+              P.S. {emailStatus === "success" ? " I'll get an email with your response" : 
+                " Jarred's stupid bad code messed up and the email sending didn't work - so just text him your response!"}
             </div>
           </div>
-          <button style={{maxWidth: "100px", width: "100px"}}>Yes</button><br />
-          <button style={{maxWidth: "100px", width: "100px"}} onClick={() => handleClick()}>No</button><br />
-          <div className="alert hide">❌ I'm sorry, that answer isn't allowed, try a different one ❌</div>
-        </div>
-      </div>
-      <div className={`step-holder ${step >= 5 ? "hide" : ""}`}>
-        <button onClick={() => setStep(1)}>Okay ask away...</button>
-        <div className={`accordion ${step >= 1 ? "show" : ""}`}>
-          <p>
-            I wanted to do something cooler, but this was the best I could come up with...
-          </p>
-          <button onClick={() => {
-            setStep(2);
-            setTimeout(() => {
-              setStep(3);
-              setTimeout(() => setStep(4), 1000);
-            }, 1500)
-          }}>No problem, carry on!</button>
-        </div>
-        <div className={`accordion ${step >= 2 ? "show" : ""}`}>
-          <p>
-            Okay here it goes...
-          </p>
-        </div>
-        <div className={`accordion ${step >= 3 ? "show" : ""}`}>
-          <p>
-            ...
-          </p>
-        </div>
-        <div className={`accordion ${step >= 4 ? "show" : ""}`}>
-          <p>
-            Are you sure you're ready for this?
-          </p>
-          <button onClick={() => setStep(5)}>Yes!</button>
-        </div>
-        <div ref={bottomRef} />
-      </div>
+      )}
+      {emailStatus === null ? (
+        <>
+          <h1>Hi {capitalize(person)},</h1>
+          {showConfetti && (
+            <div style={{position: "absolute", margin: "25% 50%"}}>
+              <ConfettiExplosion duration={5000} />
+            </div>
+          )}
+          <div className='header'>
+            <h2>I have a very important question for you...</h2>
+            <div className={`countdown ${step >= 5 && countdown !== -1 ? "" : "hide"}`}>
+              {countdown}
+            </div>
+            <div className={`question ${countdown === -1 ? "" : "hide"}`}>
+              <div className='question-text'>
+                <div className='question-text-conf'>
+                 <div>🎉</div><div>🎉</div>
+                </div>
+               <div>
+                  Will you be {resolveRole(person)}?
+               </div>
+                <div className='question-text-conf'>
+                 <div>🎉</div><div>🎉</div>
+               </div>
+              </div>
+              <button style={{maxWidth: "100px", width: "100px"}} onClick={() => sendEmailNotification()}>Yes</button><br />
+              <button style={{maxWidth: "100px", width: "100px"}} onClick={() => handleClick()}>No</button><br />
+              <div className="alert hide">❌ I'm sorry, that answer isn't allowed, try a different one ❌</div>
+            </div>
+          </div>
+          <div className={`step-holder ${step >= 5 ? "hide" : ""}`}>
+            <button onClick={() => setStep(1)}>Okay ask away...</button>
+            <div className={`accordion ${step >= 1 ? "show" : ""}`}>
+              <p>
+               I wanted to do something cooler, but this was the best I could come up with...
+              </p>
+              <button onClick={() => {
+                setStep(2);
+                setTimeout(() => {
+                  setStep(3);
+                  setTimeout(() => setStep(4), 1000);
+                }, 1500)
+             }}>No problem, carry on!</button>
+          </div>
+            <div className={`accordion ${step >= 2 ? "show" : ""}`}>
+             <p>
+               Okay here it goes...
+             </p>
+           </div>
+            <div className={`accordion ${step >= 3 ? "show" : ""}`}>
+             <p>
+               ...
+             </p>
+            </div>
+            <div className={`accordion ${step >= 4 ? "show" : ""}`}>
+             <p>
+               Are you sure you're ready for this?
+             </p>
+             <button onClick={() => setStep(5)}>Yes!</button>
+            </div>
+            <div ref={bottomRef} />
+          </div>
+        </>
+      ) : null}
     </>
   )
 }
